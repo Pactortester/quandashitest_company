4479b9a1b9f797af802a4544583d86ae9b780b85
QDS <1456470136@qq.com>
2018-07-27 16:04:01 +0800

    增加订单备注
    """订单备注"""
            self.driver.find_element_by_css_selector(
                "html body div.smartRegister-page div.orderinfo-wrap div.order-content textarea").send_keys(
                time.strftime("%Y-%m-%d_%H-%M-%S") + "测试订单")

--- case/znzztest.py
+++ case/znzztest.py
@@ -110,14 +110,19 @@ class znzztest(mytestcase):
             "132132@qq.com")
         self.driver.find_element_by_xpath(
             "/html/body/div[6]/div[3]/div[2]/div[1]/table[2]/tbody[1]/tr[4]/td[2]/input").send_keys("03515978787")
-        time.sleep(4)
+        time.sleep(5)
 
         self.driver.find_element_by_css_selector(
             "body > div.smartRegister-page > div.agentInfo-wrap.applicant-wrap > div.section-base > div.section-btns.clearfix > a:nth-child(2)").click()
         time.sleep(1)
+        # self.driver.find_element_by_css_selector(
+        #     "body > div.smartRegister-page > div.agentInfo-wrap.applicant-wrap > div.section-base > div.section-btns.clearfix > a:nth-child(2)").click()
+        # time.sleep(1)
+        """订单备注"""
         self.driver.find_element_by_css_selector(
-            "body > div.smartRegister-page > div.agentInfo-wrap.applicant-wrap > div.section-base > div.section-btns.clearfix > a:nth-child(2)").click()
-        time.sleep(1)
+            "html body div.smartRegister-page div.orderinfo-wrap div.order-content textarea").send_keys(
+            time.strftime("%Y-%m-%d_%H-%M-%S") + "测试订单")
+
         get_screenshort(self.driver, "znzztest.png")
 
         for i in self.driver.find_elements_by_css_selector("body > div.smartRegister-page > div.orderinfo-wrap > div.last-pay.personal-last-pay > ul > li.row-sense > em > i"):
