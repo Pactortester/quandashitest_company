dd43469be204dd1d9066f4f7d90d6482a75f8953
QDS <1456470136@qq.com>
2018-07-31 08:48:30 +0800

    更新cookie

--- case/mfsbtest.py
+++ case/mfsbtest.py
@@ -35,7 +35,7 @@ class gnsbtest(mytestcase):
         print(self.driver.title)
 
         self.driver.add_cookie({'name': 'QDS_COOKIE',
-                                'value': '8cf92a5f3d7ecd1b68462f4b8c95391052c3efe3',  # 一周有效期  2018-7-9_10-46
+                                'value': '679b328b686905c5b148b5fc90f5db46cacf89a3',  # 一周有效期  2018-7-9_10-46
                                 'Domain': '.quandashi.com'})
 
         self.driver.refresh()
--- utils/logincookie.py
+++ utils/logincookie.py
@@ -16,7 +16,7 @@ class dengLuPage:
         self.open_page()
 
         self.driver.add_cookie({'name': 'QDS_COOKIE',
-              'value': '8cf92a5f3d7ecd1b68462f4b8c95391052c3efe3', #一周有效期      mfsbtest.py
+              'value': '679b328b686905c5b148b5fc90f5db46cacf89a3', #一周有效期      mfsbtest.py
               'Domain': '.quandashi.com'})
 
         self.driver.refresh()
